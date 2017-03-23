8b4073c738388227f4d7b90cda8906089397eca6
Daniel Walldorf <d.walldorf@me.com>
2017-03-23 20:22:41 +0100

    dummy test

---
+++ src/test/java/com/dwalldorf/trackmytime/rest/controller/VersionControllerTest.java
@@ -0,0 +1,16 @@
+package com.dwalldorf.trackmytime.rest.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VersionControllerTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        assertTrue(true);
+    }
+}
