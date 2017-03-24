34f8b32816c7953f326f3241339ed81b0baad744
Daniel Walldorf <d.walldorf@me.com>
2017-03-24 14:45:29 +0100

    create reigstration template

--- src/main/java/com/dwalldorf/trackmytime/config/WebMvcConfig.java
+++ src/main/java/com/dwalldorf/trackmytime/config/WebMvcConfig.java
@@ -10,6 +10,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("index");
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("user/login");
+        registry.addViewController("/register").setViewName("user/register");
     }
 }
