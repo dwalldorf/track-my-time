02d8e0a7b53571457cdea4b8ac1367fee3cdf5e4
Daniel Walldorf <d.walldorf@me.com>
2017-03-24 19:28:43 +0100

    config cleanup

--- src/main/java/com/dwalldorf/trackmytime/config/WebSecurityConfig.java
+++ src/main/java/com/dwalldorf/trackmytime/config/WebSecurityConfig.java
@@ -1,34 +1,20 @@
 package com.dwalldorf.trackmytime.config;
 
-import com.dwalldorf.trackmytime.service.UserService;
 import javax.inject.Inject;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
-
-    private final PasswordEncoder passwordEncoder;
+    private final DaoAuthenticationProvider authenticationProvider;
 
     @Inject
-    public WebSecurityConfig(UserService userService, PasswordEncoder passwordEncoder) {
-        this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userService);
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
+    public WebSecurityConfig(DaoAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
     }
 
     @Override
@@ -44,6 +30,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+        auth.authenticationProvider(authenticationProvider);
     }
 }
