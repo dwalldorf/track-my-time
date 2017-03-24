d92423acca7e61f5d2b09cc771ac7a5625354cf7
Daniel Walldorf <d.walldorf@me.com>
2017-03-24 15:31:42 +0100

    introduce LoginController

---
+++ src/main/java/com/dwalldorf/trackmytime/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.dwalldorf.trackmytime.controller;
+
+import com.dwalldorf.trackmytime.forms.user.LoginForm;
+import com.dwalldorf.trackmytime.forms.user.RegisterForm;
+import com.dwalldorf.trackmytime.service.UserService;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Inject
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid LoginForm loginForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return loginPage();
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid RegisterForm registerForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return registerPage();
+        }
+        return "redirect:/login";
+    }
+}
