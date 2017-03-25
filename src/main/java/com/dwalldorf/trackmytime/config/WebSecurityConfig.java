6eb64faddaac7b625ee34b80ee06e08aeb380c71
Daniel Walldorf <d.walldorf@me.com>
2017-03-25 18:32:40 +0100

    allow unauthorized access to /vendor

--- src/main/java/com/dwalldorf/trackmytime/config/WebSecurityConfig.java
+++ src/main/java/com/dwalldorf/trackmytime/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/login", "/register").permitAll()
+            .antMatchers("/login", "/register", "/vendor/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin().loginPage("/login")
