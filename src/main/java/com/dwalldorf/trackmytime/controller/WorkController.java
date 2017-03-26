aab27fa74370d81aa8a7f6bc4bfefce062af8f32
Daniel Walldorf <d.walldorf@me.com>
2017-03-26 14:33:36 +0200

    wip

--- src/main/java/com/dwalldorf/trackmytime/controller/WorkController.java
+++ src/main/java/com/dwalldorf/trackmytime/controller/WorkController.java
@@ -3,9 +3,11 @@ package com.dwalldorf.trackmytime.controller;
 import com.dwalldorf.trackmytime.model.WorkEntry;
 import com.dwalldorf.trackmytime.service.WorkEntryService;
 import javax.inject.Inject;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +24,11 @@ public class WorkController {
     @GetMapping("/add")
     public String addPage(@ModelAttribute WorkEntry workEntry) {
         return "work/edit";
+    }
+
+    @PostMapping("/saveEntry")
+    public String add(@ModelAttribute @Valid WorkEntry workEntry) {
+        System.out.println();
+        return addPage(workEntry);
     }
 }
