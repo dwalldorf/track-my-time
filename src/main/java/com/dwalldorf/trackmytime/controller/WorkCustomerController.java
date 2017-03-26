77ade106409d3722da91bc758e893c88a2f58700
Daniel Walldorf <d.walldorf@me.com>
2017-03-26 18:53:57 +0200

    introduce WorkCustomerController

---
+++ src/main/java/com/dwalldorf/trackmytime/controller/WorkCustomerController.java
@@ -0,0 +1,12 @@
+package com.dwalldorf.trackmytime.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/work/customer")
+public class WorkCustomerController {
+
+
+
+}
