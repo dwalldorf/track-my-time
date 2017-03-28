7be905801d363f8875d185492feb940ae53599fe
Daniel Walldorf <d.walldorf@me.com>
2017-03-28 20:50:50 +0200

    unit test stub WorkProjectController

---
+++ src/test/java/com/dwalldorf/trackmytime/controller/WorkProjectControllerTest.java
@@ -0,0 +1,29 @@
+package com.dwalldorf.trackmytime.controller;
+
+import com.dwalldorf.trackmytime.BaseTest;
+import org.junit.Test;
+
+public class WorkProjectControllerTest extends BaseTest {
+
+    @Test
+    public void testEditPage_VerifiesResourceOwner() {
+    }
+
+    @Test
+    public void testSave_NewProject() throws Exception {
+
+    }
+
+    @Test
+    public void testSave_UpdateProject() throws Exception {
+
+    }
+
+    @Test
+    public void testSave_VerifiesResourceOwner() {
+    }
+
+    @Test
+    public void testDelete_VerifiesResourceOwner() {
+    }
+}
