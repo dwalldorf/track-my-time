6f01d3a0f81f45bf0d8bfd474ffa963d43d57b76
Daniel Walldorf <d.walldorf@me.com>
2017-03-28 20:47:13 +0200

    unit test WorkCustomerController

--- src/test/java/com/dwalldorf/trackmytime/controller/WorkCustomerControllerTest.java
+++ src/test/java/com/dwalldorf/trackmytime/controller/WorkCustomerControllerTest.java
@@ -1,5 +1,7 @@
 package com.dwalldorf.trackmytime.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
 
@@ -8,6 +10,7 @@ import com.dwalldorf.trackmytime.model.Customer;
 import com.dwalldorf.trackmytime.service.CustomerService;
 import com.dwalldorf.trackmytime.service.UserService;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
 public class WorkCustomerControllerTest extends BaseTest {
@@ -31,14 +34,62 @@ public class WorkCustomerControllerTest extends BaseTest {
         Customer mockPersistedCustomer = new Customer().setId(id);
         when(mockCustomerService.findById(eq(id))).thenReturn(mockPersistedCustomer);
 
-
         customerController.editPage(id);
 
         verify(mockUserService).verifyOwner(eq(mockPersistedCustomer));
     }
 
     @Test
-    public void save() {
+    public void testSave_NewCustomer() {
+        final String mockCurrentUserId = "58d7f5895ff8d846183ebbcb";
+        when(mockUserService.getCurrentUserId()).thenReturn(mockCurrentUserId);
+        Customer customer = new Customer();
+
+        customerController.save(customer);
+
+        ArgumentCaptor<Customer> customerCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(mockCustomerService).save(customerCaptor.capture());
+
+        Customer capturedCustomer = customerCaptor.getValue();
+        assertNotNull(capturedCustomer.getUserId());
+        assertEquals(mockCurrentUserId, capturedCustomer.getUserId());
+        assertEquals(customer.getName(), capturedCustomer.getName());
+    }
+
+    @Test
+    public void testSave_UpdateCustomer() {
+        final String mockCurrentUserId = "58d7f5895ff8d846183ebbcb";
+        when(mockUserService.getCurrentUserId()).thenReturn(mockCurrentUserId);
+        Customer customer = new Customer();
+        customer.setId("2c11acf45ff8d8461834f299")
+                 .setUserId(mockCurrentUserId);
+
+        when(mockCustomerService.findById(eq(customer.getId()))).thenReturn(customer);
+
+        customerController.save(customer);
+
+        ArgumentCaptor<Customer> customerCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(mockCustomerService).save(customerCaptor.capture());
+
+        Customer capturedCustomer = customerCaptor.getValue();
+        assertEquals(mockCurrentUserId, capturedCustomer.getUserId());
+        assertEquals(customer.getName(), capturedCustomer.getName());
+    }
+
+    @Test
+    public void testSave_VerifiesResourceOwner() {
+        final String mockCurrentUserId = "58d7f5895ff8d846183ebbcb";
+        final String originalUserId = "2c11acf45ff8d8461834f299";
+        when(mockUserService.getCurrentUserId()).thenReturn(mockCurrentUserId);
+
+        Customer customer = new Customer()
+                .setId("2c11acf45ff8d8461834f299")
+                .setUserId(originalUserId);
+        when(mockCustomerService.findById(eq(customer.getId()))).thenReturn(customer);
+
+        customerController.save(customer);
+
+        verify(mockUserService).verifyOwner(eq(customer));
     }
 
     @Test
