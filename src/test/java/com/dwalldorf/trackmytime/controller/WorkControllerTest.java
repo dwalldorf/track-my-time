a27924cb5033944b4adcab99056562f1d99fc12d
Daniel Walldorf <d.walldorf@me.com>
2017-03-28 19:12:42 +0200

    introduce WorkControllerTest

---
+++ src/test/java/com/dwalldorf/trackmytime/controller/WorkControllerTest.java
@@ -0,0 +1,15 @@
+package com.dwalldorf.trackmytime.controller;
+
+import com.dwalldorf.trackmytime.BaseTest;
+import org.junit.Test;
+
+public class WorkControllerTest extends BaseTest {
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+}
