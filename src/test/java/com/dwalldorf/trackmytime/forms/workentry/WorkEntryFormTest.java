7caf444ca0ff3f176b14c2b369d48ab5031c1597
Daniel Walldorf <d.walldorf@me.com>
2017-03-29 23:02:44 +0200

    tests - wip

---
+++ src/test/java/com/dwalldorf/trackmytime/forms/workentry/WorkEntryFormTest.java
@@ -0,0 +1,19 @@
+package com.dwalldorf.trackmytime.forms.workentry;
+
+import static org.junit.Assert.fail;
+
+import com.dwalldorf.trackmytime.BaseTest;
+import org.junit.Test;
+
+public class WorkEntryFormTest extends BaseTest {
+
+    @Test
+    public void toWorkEntry() throws Exception {
+        fail("implement me");
+    }
+
+    @Test
+    public void fromWorkEntry() throws Exception {
+        fail("implement me");
+    }
+}
