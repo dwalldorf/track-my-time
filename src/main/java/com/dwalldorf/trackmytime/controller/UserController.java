37bacf063c671d519068be12c09994ddd682d3b6
Daniel Walldorf <d.walldorf@me.com>
2017-03-31 16:43:49 +0200

    edit user

--- src/main/java/com/dwalldorf/trackmytime/controller/UserController.java
+++ src/main/java/com/dwalldorf/trackmytime/controller/UserController.java
@@ -2,7 +2,12 @@ package com.dwalldorf.trackmytime.controller;
 
 import com.dwalldorf.trackmytime.exception.InvalidFormInputException;
 import com.dwalldorf.trackmytime.forms.user.RegisterForm;
+import com.dwalldorf.trackmytime.model.Customer;
+import com.dwalldorf.trackmytime.model.User;
+import com.dwalldorf.trackmytime.service.CustomerService;
 import com.dwalldorf.trackmytime.service.UserService;
+import com.dwalldorf.trackmytime.util.RouteUtil;
+import java.util.List;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -11,15 +16,26 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
+    // TODO: make constants for view paths
+
     private final UserService userService;
 
+    private final CustomerService customerService;
+
     @Inject
-    public UserController(UserService userService) {
+    public UserController(UserService userService, CustomerService customerService) {
         this.userService = userService;
+        this.customerService = customerService;
+    }
+
+    @ModelAttribute("allCustomers")
+    public List<Customer> allCustomers() {
+        return customerService.findAllByUser(userService.getCurrentUserId());
     }
 
     @GetMapping("/register")
@@ -42,6 +58,22 @@ public class UserController {
             return registerPage(registerForm);
         }
 
+        // TODO: use RouteUtil
         return "redirect:/login";
+    }
+
+    @GetMapping("/user/edit")
+    public ModelAndView editPage() {
+        ModelAndView mav = new ModelAndView("user/edit");
+
+        User user = userService.getCurrentUser();
+        mav.addObject("user", user);
+
+        return mav;
+    }
+
+    @PostMapping("/user/edit")
+    public String update() {
+        return RouteUtil.redirectString("/user/edit");
     }
 }
