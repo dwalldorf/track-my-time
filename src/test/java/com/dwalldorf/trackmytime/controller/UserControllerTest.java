ba4a118ce8223025dffe9ab5cd03fee7262fe3da
Daniel Walldorf <d.walldorf@me.com>
2017-04-01 14:56:47 +0200

    unit tests wip

---
+++ src/test/java/com/dwalldorf/trackmytime/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.dwalldorf.trackmytime.controller;
+
+import static org.junit.Assert.fail;
+
+import com.dwalldorf.trackmytime.BaseTest;
+import org.junit.Test;
+
+public class UserControllerTest extends BaseTest {
+
+    @Test
+    public void testRegister() {
+        fail("write test");
+    }
+
+    @Test
+    public void testEditPage() {
+        fail("write test");
+    }
+
+    @Test
+    public void testUpdate() {
+        fail("write test");
+    }
+}
