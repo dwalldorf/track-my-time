2f6b1a49f9a21bc6f48bbe3e35c70c07b7781406
Daniel Walldorf <d.walldorf@me.com>
2017-04-01 15:07:29 +0200

    unit test editPage

--- src/test/java/com/dwalldorf/trackmytime/controller/UserControllerTest.java
+++ src/test/java/com/dwalldorf/trackmytime/controller/UserControllerTest.java
@@ -1,11 +1,30 @@
 package com.dwalldorf.trackmytime.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import com.dwalldorf.trackmytime.BaseTest;
+import com.dwalldorf.trackmytime.service.CustomerService;
+import com.dwalldorf.trackmytime.service.UserService;
+import java.util.Map;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class UserControllerTest extends BaseTest {
+
+    @Mock
+    private UserService mockUserService;
+
+    @Mock
+    private CustomerService mockCustomerService;
+
+    private UserController userController;
+
+    @Override
+    protected void setUp() {
+        this.userController = new UserController(mockUserService, mockCustomerService);
+    }
 
     @Test
     public void testRegister() {
@@ -13,8 +32,19 @@ public class UserControllerTest extends BaseTest {
     }
 
     @Test
-    public void testEditPage() {
-        fail("write test");
+    public void testEditPage_Model() {
+        final String expectedUserModelKey = "user";
+        final Map<String, Object> model = userController.editPage().getModel();
+
+        assertTrue(model.containsKey(expectedUserModelKey));
+    }
+
+    @Test
+    public void testEditPage_View() {
+        final String expectedView = "/user/edit";
+        final String viewName = userController.editPage().getViewName();
+
+        assertEquals(expectedView, viewName);
     }
 
     @Test
